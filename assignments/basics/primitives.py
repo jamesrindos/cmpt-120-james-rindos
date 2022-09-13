ea2d43c7fe80089f84ba7d0b78400d8e897a9a5c
James Rindos <jamesrindos@148-100-137-159.foxnet.marist.edu>
2022-09-12 21:33:27 -0400

    added basics folder

---
+++ assignments/basics/primitives.py
@@ -0,0 +1,27 @@
+from pickletools import pybytes_or_str
+from xxlimited import Str
+
+
+def get_int():
+    '''
+    Returns a variable of type 'int'.
+    '''
+    return 9
+
+def get_float():
+    '''
+    Returns a variable of type 'float'.
+    '''
+    return 3.18
+
+def get_str():
+    '''
+    Returns a variable of type 'str'.
+    '''
+    return "Hello"
+
+def get_bool():
+    '''
+    Returns a variable of type 'bool'.
+    '''
+    return True
